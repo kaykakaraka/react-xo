c97785344f7b409ab8cd282da5758e95fc631bf2
Kay <kard@hotmail.co.uk>
2022-08-26 10:35:21 +0100

    Adds X to the box when clicked

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ class Square extends React.Component {
     super(props);
     this.state = { value: null };
   }
-  
+
   render() {
     return (
-      <button className="square" onClick={() => {console.log('clicked')}}>
-        {this.props.value}
+      <
+        button className="square" 
+        onClick={() => {this.setState({value: 'X'})}}
+      >
+        {this.state.value}
       </button>
     );
   }
